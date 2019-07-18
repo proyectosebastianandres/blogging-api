34144255405491842effb8310c421b5ab8332c0c
Andres <sernmandres@gmail.com>
2019-07-17 19:15:44 -0500

    archivo .env.example datos de ejemplo para pruebas se agrega .env en el .gitignore

---
+++ config/environment.js
@@ -0,0 +1,21 @@
+const all = {
+  env: process.env.NODE_ENV,
+  // Server port
+  port: process.env.PORT || 8080,
+  // Server IP
+  ip: process.env.IP || '127.0.0.1',
+  // Should we populate the DB with sample data?
+  seedDB: false,
+  // Secret for session, you will want to change this and make it an environment variable
+  secrets: {
+    session: 'bloggin-api',
+  },
+
+  // MongoDB connection options
+  mongo: {
+    uri: process.env.MONGODB_URI,
+    db: 'bloggin-api',
+  },
+};
+
+module.exports = all;
